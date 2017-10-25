49299abaa8dac02872aed17fe7cf6e6c7f56d082
Alejandro Villarreal <avillarreal01@gmail.com>
2017-10-25 17:31:18 -0400

    edits

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getEntries } from '../store'
+
+import FeedEntryList from '../components/FeedEntryList'
 
 class Feed extends Component {
+  componentDidMount () {
+    this.props.getEntries('http://www.reddit.com/.rss')
+  }
+
   render () {
-    return (
-      <ul>
-        <li>Some stuff</li>
-        <li>Some stuff</li>
-        <li>Some stuff</li>
-        <li>Some stuff</li>
-      </ul>
-    )
+    console.log(this.props)
+    if (this.props.feedEntries.length) {
+      return <FeedEntryList entries={this.props.feedEntries} />
+    }
+    return <h3>Loading...</h3>
   }
 }
 
-export default Feed
+const mapState = state => ({
+  feedEntries: state.feedEntries
+})
+
+const mapDispatch = { getEntries }
+
+export default connect(mapState, mapDispatch)(Feed)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 
 import './styles/index.css'
+import store from './store'
 import App from './containers/App'
 import registerServiceWorker from './utils/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
